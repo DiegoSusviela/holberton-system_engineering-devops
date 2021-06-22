8b920485e2c089c498e5bc9d6d1a7281db263c58
DiegoSusviela <dieguitosus@hotmail.com>
2021-06-21 23:40:44 -0300

    adding zombies

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - zasdasd
+ *
+ * Description: asdasda
+ * Return: asdasd
+ */
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+/**
+ * main - zasdasd
+ *
+ * Description: asdasda
+ * Return: asdasd
+ */
+
+int main(void)
+{
+	int horda_zombies;
+	pid_t zombie;
+
+	for (horda_zombies = 0; horda_zombies < 5; horda_zombies++)
+	{
+		zombie = fork();
+		if (!zombie)
+			exit(0);
+		printf("Zombie process created, PID: %i\n", zombie);
+	}
+	infinite_while();
+	return (0);
+}
