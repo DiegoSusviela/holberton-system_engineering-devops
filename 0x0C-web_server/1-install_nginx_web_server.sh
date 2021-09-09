2d3664dc9489dbe66514f9751366f51d52483a15
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-08 22:32:47 -0300

    updated

--- 0x0C-web_server/1-install_nginx_web_server.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# aewgegiuhif qibwe
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-echo "Holberton School" | sudo tee /usr/share/nginx/html/index.html
-sudo service nginx start
