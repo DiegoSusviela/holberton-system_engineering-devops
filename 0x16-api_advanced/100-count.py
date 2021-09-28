4145b56e3b63c1c92f283755d56cf8a087cd3a37
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-28 20:23:41 -0300

    addedadvanced

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Reddit API"""
+import requests
+
+def count_words(subreddit, word_list, pagination="", results={}, count=0):
+	headers = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/' + subreddit +\
+          '/hot.json?after={}'.format(pagination)
+	r_u = requests.get(url, headers=headers, allow_redirects=False)
+	if r_u.status_code != 200:
+        return None
+	res = r_u.json()
+	hot = res.get('data').get('children')
+	for pos in hot:
+        tit = pos.get('data').get('title'))
+		for pos2 in word_list:
+                for pos3 in tit.split():
+                    if pos2.lower() in pos3.lower():
+                        cont += 1
+                results[i] = cont
+            cont = 0
+    pagination = res.get('data').get('after')
+    if pagination is not None:
+        count_words(subreddit, word_list, pagination, results, count)
+	else:
+		for key, value in results.items():
+			print("{}: {}".format(key, value))
