41651e3c3b3bf3d56331377414d702538861f255
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-28 20:02:26 -0300

    changing request in 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
-    r_u = requests.get('https://www.reddit.com/r/{:}/about.json'.format(
-        subreddit), headers=headers, allow_redirects=False)
+    r_u = requests.get('https://www.reddit.com/r/' +
+                       subreddit + '/about.json',
+                       headers=headers, allow_redirects=False)
     if r_u.status_code == 200:
         usr = req_users.json().get('data')
 		return usr.get('subscribers')
