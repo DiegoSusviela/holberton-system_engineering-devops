01f98cac69262d4d2df579c397b59955d6767968
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-28 23:59:17 -0300

    restaged changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     r_u = requests.get('https://www.reddit.com/r/' +
                        subreddit + '/hot.json?limit=10',
                        headers=headers, allow_redirects=False)
-    if r_u.status_code <= 300:
+    if r_u.status_code < 300:
         json = r_u.json()
         data_dict = json.get('data')
         hot = data_dict.get('children')
